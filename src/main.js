b698973e454700921bdc99ac11d037c76ba134d4
John Philip <developerphilo@gmail.com>
2022-04-27 01:07:16 +0300

    firebase initialization refactor

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from "./App.vue";
 import AppDate from "@/components/AppDate";
 import store from "@/store/index";
 import router from "@/router";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import firebaseConfig from "./config/firebase";
-// Your web app's Firebase configuration// Initialize Firebase
-initializeApp(firebaseConfig);
+
+import firebase from "firebase/compat/app";
+import firebaseConfig from "@/config/firebase.js";
+
+firebase.initializeApp(firebaseConfig);
 const forumApp = createApp(App);
 forumApp.use(router);
 forumApp.use(store);
