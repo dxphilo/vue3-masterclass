0c8ab38c4c675ee60602b28529a419430cac6116
John Philip <developerphilo@gmail.com>
2022-04-26 12:00:20 +0300

    feat: intergrate firebase into the forum application

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+module.exports = {
+  apiKey: "AIzaSyDM_LmPsvHo_kSczaug_QpmRTAxhdmiroY",
+  authDomain: "vue3-masterclass-b01ab.firebaseapp.com",
+  projectId: "vue3-masterclass-b01ab",
+  storageBucket: "vue3-masterclass-b01ab.appspot.com",
+  messagingSenderId: "674744659035",
+  appId: "1:674744659035:web:788b6c349aacb09340237e",
+  measurementId: "G-LHJVJQ8YJZ",
+};
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import AppDate from "@/components/AppDate";
 import store from "@/store/index";
 import router from "@/router";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "@/config/firebase";
+// Initialize Firebase
+initializeApp(firebaseConfig);
 const forumApp = createApp(App);
 forumApp.use(router);
 forumApp.use(store);
